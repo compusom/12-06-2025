8c1f0b494e363ec43ce9f1f9e8d258766df2bc8d
compusom <compusom@gmail.com>
2025-06-12 20:29:18 -0300

    Delete data_processing directory

--- data_processing/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Data processing utilities for the report generator project.
-
-This package groups modules for loading raw data, aggregating statistics,
-calculating metrics and orchestrating the creation of formatted reports.
-"""
-
